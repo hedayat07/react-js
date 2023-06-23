4369c231e187e94af27079551440d16718d39d14
hedayat07 <97403589+hedayat07@users.noreply.github.com>
2023-06-23 15:55:37 +0530

    comment remove

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,25 +5,21 @@ import { useState } from 'react'
 export default function TextForm(props) {
 
     const handleupClick = () =>{
-        //console.log("Upper case was clicked" + text);
         let newText = text.toUpperCase();
         setText(newText)
     }
 
     const handlelowerClick = () =>{
-        //console.log("LOWER case was clicked" + text);
         let newText = text.toLowerCase();
         setText(newText)
     }
     const handleclearClick = () =>{
-        //console.log("clear clicked" + text);
         let newText = "";
         setText(newText)
         
     }
 
     const handleonChange = (event)=>{
-       //console.log("on change");
         setText(event.target.value)
     }
     const [text,setText]= useState("")
